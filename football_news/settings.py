0cc93d9479192cd1ccbbb6540ee234475247f121
unknown <fini.ardiyanti@gmail.com>
2025-09-10 14:38:50 +0700

    tutorial 2 selesai

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "tsaniya-fini-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://tsaniya-fini-footballnews.pbp.cs.ui.ac.id/"
+    "https://tsaniya-fini-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
